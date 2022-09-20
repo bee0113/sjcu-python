4f6124098ef6d598dfcada83e9f7eac5d005e7c6
mac <berno579@gmail.com>
2022-09-20 22:34:09 +0900

    4주차 실습

---
+++ 4weeks/questions/Q1.py
@@ -0,0 +1,21 @@
+# 이번 학기 이수 과목의 점수를 입력 받고 합계를 출력하기
+# 일반변수
+# hap=0
+# a = int(input('num1:'))
+# b = int(input('num2:'))
+# c = int(input('num3:'))
+# d = int(input('num4:'))
+# # 총합계
+# hap = a + b + c + d
+# print('hap', hap)
+
+# 리스트 변수
+a = [10, 10, 10, 10]
+a[0] = int(input('num1:'))
+a[1] = int(input('num2:'))
+a[2] = int(input('num3:'))
+a[3] = int(input('num4:'))
+print(a)
+hap = a[0] + a[1] + a[2] + a[3]
+print('hap', hap, sep='')
+print('hap', hap)
