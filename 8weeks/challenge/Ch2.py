868a9caa69d8f892bfeb62effde93ddb527ed66f
mac <berno579@gmail.com>
2022-12-04 17:23:43 +0900

    8주차 실습 조건문 소괄호 삭제

--- 8weeks/challenge/Ch2.py
+++ 8weeks/challenge/Ch2.py
@@ -8,12 +8,12 @@ print("<<컴퓨터가 생각하는 1~20 숫자 맞추기>>")
 # print("난수 %d" % com)
 while True:
     player = int(input("숫자 입력(종료 0): "))
-    if (player == 0):
+    if player == 0:
         break
-    elif (player == com):
+    elif player == com:
         print("정답!")
         break
-    elif (player > com):
+    elif player > com:
         print("더 작은 숫자 입력!")
     else:
         print("더 큰 숫자 입력!")
